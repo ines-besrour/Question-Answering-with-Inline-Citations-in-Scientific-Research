229c0c471611c5eaed3566d3456af36076d9dfd1
ines besrour <87935896+ines-besrour@users.noreply.github.com>
2025-06-04 18:13:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ USE_GPU = torch.cuda.is_available()
 # Configuration paths
 DATA_DIR = "/data/horse/ws/inbe405h-unarxive/processed_unarxive_extended_data"
 DOC_INDEX_DIR = "/data/horse/ws/inbe405h-unarxive/faiss_index"
+E5_INDEX_DIR = "/data/horse/ws/inbe405h-unarxive/test_index"
+BM25_INDEX_DIR = "/data/horse/ws/inbe405h-unarxive/bm25_retriever"
+DB_PATH = "/data/horse/ws/inbe405h-unarxive/full_text_db"
